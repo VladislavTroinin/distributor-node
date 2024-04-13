11a414c79a97f0a53ad3af5644fce40a3826f13e
Vladislav Troynin <vladislav.troynin@mirapolis.ru>
2024-04-13 20:28:56 +0300

    Initial commit

---
+++ src/test/java/org/vsu/distributor/DistributorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vsu.distributor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
