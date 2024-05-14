05c826b2a30c00d588e4a3f2fa9adea980b727b9
Vladislav Troynin <vladislav.troynin@mirapolis.ru>
2024-05-14 09:25:23 +0300

    The 1st milestone

---
+++ src/main/java/org/vsu/distributor/domain/service/MigrationService.java
@@ -0,0 +1,12 @@
+package org.vsu.distributor.domain.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class MigrationService {
+
+
+
+}
